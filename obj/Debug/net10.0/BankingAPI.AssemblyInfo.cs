343178e16ec0df9974080fa5269a68adfea29046
Elijah Macharia <kabugimacharia.e23@gmail.com>
2026-02-24 17:41:47 +0300

    Add .gitignore

--- obj/Debug/net10.0/BankingAPI.AssemblyInfo.cs
+++ obj/Debug/net10.0/BankingAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankingAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2331e4261d481d5348fb5ee2a794f4c7c6683957")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f5443fb773c342918f698c3725d095fc27d559f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankingAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankingAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
